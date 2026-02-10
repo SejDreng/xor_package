b57c12b8b1dcf7d921e6f32433ffebf47d2307b9
SejDreng <adriananthony10@gmail.com>
2026-02-10 20:50:41 +0100

    Initial commit

---
+++ xor_package/client_node.py
@@ -0,0 +1,57 @@
+#!/home/adrian/ROS2_Workspaces/ros_ml_venv/bin/python3
+
+from rclpy.node import Node
+import rclpy
+
+import random
+
+# message types
+from std_msgs.msg import UInt8
+from std_msgs.msg import UInt8MultiArray
+from std_msgs.msg import Float32
+
+# service types
+from xor_package.srv import XorRequest
+
+class ml_client(Node):
+    def __init__(self):
+        super().__init__('ml_client')
+        self.cli = self.create_client(XorRequest, 'xor_service')
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('Service not available, waiting again...')
+        self.req = XorRequest.Request()
+        
+        self.send_request()
+        
+    def send_request(self):
+        input_list = [random.randint(0, 1), random.randint(0, 1)]
+        self.req.input = input_list
+        
+        self.future = self.cli.call_async(self.req)
+        
+        # Add a callback
+        self.future.add_done_callback(self.response_callback)
+        self.get_logger().info(f'Sending request: {input_list}')
+        
+    def response_callback(self, future):
+        try:
+            response = future.result()
+            self.get_logger().info(f'Received response: Loss={response.loss}')
+            self.send_request()
+        except Exception as e:
+            self.get_logger().error(f'Service call failed: {e}')
+            
+def main():
+    print('Hi from XOR_client.')
+
+    rclpy.init()
+    node = ml_client()
+    rclpy.spin(node)
+    
+    # If we ever exit the spin loop
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
---
+++ xor_package/ml_node.py
@@ -0,0 +1,95 @@
+#!/home/adrian/ROS2_Workspaces/ros_ml_venv/bin/python3
+
+from rclpy.node import Node
+import rclpy
+import torch
+import torch.nn as nn
+from torch import Tensor
+import torchvision.transforms as transforms
+from torchvision.transforms import ToTensor
+import torchvision.transforms.functional as F
+import torchvision
+
+# message types
+from std_msgs.msg import UInt8
+from std_msgs.msg import UInt8MultiArray
+from std_msgs.msg import Float32
+
+# service types
+from xor_package.srv import XorRequest
+
+topic_result = 'topic_result'
+
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+else:    
+    device = torch.device('cpu')
+
+class XOR_model(nn.Module):
+    def __init__(self):
+        super(XOR_model, self).__init__()
+        self.linear1 = nn.Linear(2, 4)
+        self.linear15 = nn.Linear(4, 2)
+        self.linear2 = nn.Linear(2, 1)
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = torch.relu(self.linear1(x))
+        x = torch.relu(self.linear15(x))
+        x = torch.sigmoid(self.linear2(x))
+        return x
+
+class ml_server(Node):
+    def __init__(self):
+        super().__init__('ml_server')
+        self.model = XOR_model()
+        self.model.to(device)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.1)
+        self.criterion = nn.BCELoss()
+        self.srv = self.create_service(XorRequest, 'xor_service', self.xor_callback)
+        self.publisher_ = self.create_publisher(UInt8, topic_result, 10)
+
+    def xor_callback(self, request, response):
+            if len(request.input) != 2:
+                self.get_logger().error("Input must have exactly 2 elements")
+                return response
+            
+            input_tensor = torch.tensor([request.input], dtype=torch.float32).to(device)
+            
+            target_value = request.input[0] ^ request.input[1]
+            target = torch.tensor([[target_value]], dtype=torch.float32).to(device)
+
+            ar = self.predict_and_backward(input_tensor, target)
+            response.loss = ar[0] 
+
+            result = 1 if ar[1] >= 0.5 else 0
+            
+            msg = UInt8()
+            msg.data = result
+            self.publisher_.publish(msg)
+
+            self.get_logger().info(f"Input: {request.input}, Predicted: {result}, Loss: {ar[0]}")
+                
+            return response
+        
+    def predict_and_backward (self, input_tensor: Tensor, target: Tensor) -> list[float]:
+        output = self.model(input_tensor)
+        loss = self.criterion(output, target)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        return [loss.item(), output.item()]
+
+
+def main():
+    print('Hi from XOR_server.')
+
+    rclpy.init()
+    node = ml_server()
+    rclpy.spin(node)
+
+
+
+if __name__ == '__main__':
+    main()
